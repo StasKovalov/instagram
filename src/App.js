0f6de5ca25b74362f4ad48c749ec3461a7d99c15
Valentin Stratovich <valikstratovich@gmail.com>
2019-08-11 23:00:42 +0300

    Bad check is auth

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import ProfilePage from "@pages/ProfilePage";
 import Main from "@pages/Main";
 
 /*
+
+const isAuth = LocaleStorage.getItem("isAuth");
+
 const App = () => (
   <Layout>
     <Switch>
+      <Router path="/" render={() => <Redirect to={isAuth ? "Main" : "Login"} />} />
       <Main/>
       <Login/>
       <Profile/>
