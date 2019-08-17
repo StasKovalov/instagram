58a14a8bcbe29ce98410d985d5da035f09c1a623
Stas <stasskovalov@gmail.com>
2019-08-17 17:05:58 +0300

    fsfsd

--- src/components/atoms/Photo/Photo.js
+++ src/components/atoms/Photo/Photo.js
@@ -1,15 +1,9 @@
 import React from "react";
 import style from './index.module.scss';
 
-const classNames = require('classnames');
-
-const Photo = ({ profilePhoto, mainPhoto,  children }) => {
-    const photoStyle = classNames({
-        [style.profilePhoto]: profilePhoto,
-        [style.mainPhoto]: mainPhoto,
-    })
+const Photo = ({ profilePhoto,  children }) => {
     return (
-        <div className={photoStyle}>
+        <div className={style.profilePhoto}>
             {children}
         </div>
     )
