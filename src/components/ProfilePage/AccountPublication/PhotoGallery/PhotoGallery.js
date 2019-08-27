8e3c9566641f6a1d7e534ec3c2bc84e05c4dff17
Stas <stasskovalov@gmail.com>
2019-08-28 00:14:25 +0300

    add backdrop info-counts for photo gallery in profile-page

--- src/components/ProfilePage/AccountPublication/PhotoGallery/PhotoGallery.js
+++ src/components/ProfilePage/AccountPublication/PhotoGallery/PhotoGallery.js
@@ -2,19 +2,58 @@ import React from "react";
 import style from "./index.module.scss";
 
 import Photo from "@common/Photo";
+import Icon from "@common/Icon"
 
 
 const PhotoGallery = ({ publications }) => {
 
     return (
         <div className={style.accountPhotos}>
-            {publications && publications.map((publication) => {
-                return <Photo key={publication.id} profilePhoto>
-                    <img className={style.img} src={publication.photoURL} />
-                    <div className={style.countsInfo}>
+            {publications && publications.map((publication, index) => {
+                const { counts: { likes, comments }, photoURL } = publication;
+                console.log(comments);
+                if (index < 3) {
+                    return <Photo key={publication.id} profilePhoto>
+                        <div className={style.wrapperPhoto}>
+                            <img className={style.img} src={photoURL} />
 
-                    </div>
+                            <div className={style.countsInfoWithoutTop}>
+
+                                <div className={style.counts}>
+                                    <p className={style.likeCounts}>
+                                        <Icon><span className={style.likeHeartIcon} /></Icon>
+                                        <span className={style.text}>{likes}</span>
+                                    </p>
+
+                                    <p className={style.commentCounts}>
+                                        <Icon><span className={style.commentIcon} /></Icon>
+                                        <span className={style.text}>{comments}</span>
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
                     </Photo>
+
+                }
+                return <Photo key={publication.id} profilePhoto>
+                    <div className={style.wrapperPhoto}>
+                        <img className={style.img} src={publication.photoURL} />
+                        <div className={style.countsInfo}>
+                            <div className={style.counts}>
+                                <p className={style.likeCounts}>
+                                    <Icon><span className={style.likeHeartIcon} /></Icon>
+                                    <span className={style.text}>{likes}</span>
+                                </p>
+
+                                <p className={style.commentCounts}>
+                                    <Icon><span className={style.commentIcon} /></Icon>
+                                    <span className={style.text}>{comments}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Photo>
             })}
         </div>
     )
