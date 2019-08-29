f63b2750728f8aac713b4afb8885469cc17578f7
valik <valentin.stratovich@fyutura.com>
2019-08-29 21:26:19 +0300

    fix image animation

--- src/components/ProfilePage/AccountPublication/PhotoGallery/PhotoGallery.js
+++ src/components/ProfilePage/AccountPublication/PhotoGallery/PhotoGallery.js
@@ -4,56 +4,46 @@ import style from "./index.module.scss";
 import Photo from "@common/Photo";
 import Icon from "@common/Icon";
 
-
 const PhotoGallery = ({ publications }) => {
-
-    return (
-        <div className={style.accountPhotos}>
-            {publications && publications.map((publication, index) => {
-                const { counts: { likes, comments }, photoURL } = publication;
-                if (index < 3) {
-                    return <Photo key={publication.id} profilePhoto>
-                        <div className={style.wrapperPhoto}>
-                            <img alt="user Publication" className={style.img} src={photoURL} />
-
-                            <div className={style.countsInfoWithoutTop}>
-                                <div className={style.counts}>
-                                    <div className={style.likeCounts}>
-                                        <Icon><span className={style.likeHeartIcon} /></Icon>
-                                        <span className={style.text}>{likes}</span>
-                                    </div>
-
-                                    <div className={style.commentCounts}>
-                                        <Icon><span className={style.commentIcon} /></Icon>
-                                        <span className={style.text}>{comments}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Photo>
-
-                }
-                return <Photo key={publication.id} profilePhoto>
-                    <div className={style.wrapperPhoto}>
-                        <img alt="user Publication" className={style.img} src={photoURL} />
-                        <div className={style.countsInfo}>
-                            <div className={style.counts}>
-                                <div className={style.likeCounts}>
-                                    <Icon><span className={style.likeHeartIcon} /></Icon>
-                                    <span className={style.text}>{likes}</span>
-                                </div>
-
-                                <div className={style.commentCounts}>
-                                    <Icon><span className={style.commentIcon} /></Icon>
-                                    <span className={style.text}>{comments}</span>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div className={style.accountPhotos}>
+      {publications &&
+        publications.map((publication, index) => {
+          const {
+            counts: { likes, comments },
+            photoURL
+          } = publication;
+          return (
+            <Photo key={publication.id} profilePhoto>
+              <div className={style.wrapperPhoto}>
+                <img
+                  alt="user Publication"
+                  className={style.img}
+                  src={photoURL}
+                />
+                <div className={style.countsInfo}>
+                  <div className={style.counts}>
+                    <div className={style.likeCounts}>
+                      <Icon>
+                        <span className={style.likeHeartIcon} />
+                      </Icon>
+                      <span className={style.text}>{likes}</span>
                     </div>
-                </Photo>
-            })}
-        </div>
-    )
-}
+
+                    <div className={style.commentCounts}>
+                      <Icon>
+                        <span className={style.commentIcon} />
+                      </Icon>
+                      <span className={style.text}>{comments}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Photo>
+          );
+        })}
+    </div>
+  );
+};
 
 export default PhotoGallery;
